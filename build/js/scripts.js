21954b53cc9dcc8dcbe21b79b7de16139ff56a8a
Carlos Ortega <exoticorion@gmail.com>
2014-08-13 13:17:16 -0700

    refactor script

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,60 +1,47 @@
-// Create some global variables
-var messageX     = $('.x-value'),
-    messageY     = $('.y-value'),
-    messageZ     = $('.z-value'),
-    errorText    = $('.error'),
-    operatorText = $('.operator');
+(function(){
 
-//Wrap the function inside an object to keep it out of global namespace
-var app = {
+  var messageX     = $('.x-value'),
+      messageY     = $('.y-value'),
+      messageZ     = $('.z-value'),
+      errorText    = $('.error'),
+      operatorText = $('.operator');
 
-  addOrSubtract : function(operator){
+
+  var addOrSubtract = function(operator){
     var x = $('#input-x').val(),
         y = $('#input-y').val(),
-        z = 0;
+        z = 0,
+        btnOperator = $(this).data('operator');
 
-    // If input fields are empty, surface a message.
     if (x === '' || y === ''){
       errorText.text('Oops, you forgot to input an integer!');
     }
+    else if (isNaN(x) || isNaN(y) || (x%1) !== 0 || (y%1 !==0)){
+      $('.error').text('Use integers, not letters, decimals, or special characters, please');
+    }
+    //...otherwise, proceed with calculation functions
     else{
-      //If input text are not numbers, surface a message...
-      if (isNaN(x) !== false || isNaN(y) !== false){
-        $('.error').text('Use integers, not letters or special characters, please');
+      errorText.text('');
+      x = +(x);
+      y = +(y);
+
+      if (operator === 'add'){
+        operatorText.text('plus');
+        z = x+y;
       }
-      //...otherwise, proceed with calculation functions
-      else{
-        //Clear any error messages
-        errorText.text('');
 
-        //Ensure JS is adding numbers, not strings.
-        x = parseInt(x);
-        y = parseInt(y);
-
-        //If the button pressed has a data attribute of "add", add the numbers and set the result to z
-        if (operator === 'add'){
-          operatorText.text('plus');
-          z = x+y;
-        }
-
-        //If the button pressed has a data attribute of "subtract", subtract the numbers and set the result to z
-        if (operator === 'subtract'){
-          operatorText.text('minus');
-          z = x-y;
-        }
-
-        //Update the message text in the app
-        messageX.text(x);
-        messageY.text(y);
-        messageZ.text(z);
-
+      if (operator === 'subtract'){
+        operatorText.text('minus');
+        z = x-y;
       }
+
+      messageX.text(x);
+      messageY.text(y);
+      messageZ.text(z);
+
     }
   }
-}
 
-$('.btn').click(function(){
-  //Pass the argument (determined by button's data attribute) to the function.
-  var operator = $(this).data('operator');
-  app.addOrSubtract(operator)
-});
+  $('.btn').click(addOrSubtract);
+
+}())
